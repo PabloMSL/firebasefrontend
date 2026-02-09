3cf0210c56bf1a717a2717f8f6c0bce5fac28f8a
Oscar Guacaneme <guacanemeoscarfelipe@gmail.com>
2026-02-09 17:03:14 -0500

    .

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,21 @@
 import { Routes } from '@angular/router';
-
-export const routes: Routes = [];
+import { authGuard } from './guard/auth-guard';
+export const routes: Routes = [
+  {
+    path:'',
+    redirectTo:'/auth',
+    pathMatch: 'full'
+  },{
+    path:'auth',
+    loadComponent: () => import('./components/auth/auth').then(m => m.Auth),
+    title: 'Inicio de Sesión'
+  },{
+    path:'chat',
+    loadComponent: () => import('./components/chat/chat').then(m => m.Chat),
+    title: 'Chat',
+    canActivate: [authGuard]
+  },{
+    path:'**',
+    redirectTo:'/auth'
+  }
+];
---
+++ src/app/services/firebase/firebase.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-firebase',
+  imports: [],
+  templateUrl: './firebase.html',
+  styleUrl: './firebase.css',
+})
+export class Firebase {
+
+}
