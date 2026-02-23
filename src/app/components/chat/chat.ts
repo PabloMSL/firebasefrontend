7bb1acefdb991dc7ab9748a5731e06a2ef8ae9dc
PabloMSL <jpablomilitar@gmail.com>
2026-02-23 14:35:40 -0500

    Bajar hasta el ultimo mensaje

--- src/app/components/chat/chat.ts
+++ src/app/components/chat/chat.ts
@@ -1,8 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild, ElementRef } from '@angular/core';
 import { MensajeChat } from '../../../models/chat';
 import { CommonModule } from '@angular/common';
-import { DomSanitizer, SafeHtml } from '@angular/platform-browser';
-import { FormsModule } from '@angular/forms';
+import {FormsModule} from '@angular/forms';
 
 @Component({
   selector: 'app-chat',
@@ -23,7 +22,28 @@ export class Chat {
   mensajeTexto=""
   enviandoMensaje = true
   cerrarSesion(){}
+  private debeHacerScroll = true;
 
+  @ViewChild('messagesContainer') messagesContainer! : ElementRef
+
+  private scrollhaciaabajo():void{
+    try{
+      const container = this.messagesContainer?.nativeElement
+      if(container){
+        container.scrollTop = container.scrollHeight
+      }
+    } catch(error){
+      console.error('❌ Error al hacer scroll')
+    }
+  }
+
+
+  ngAfterViewChecked():void{
+    if(this.debeHacerScroll){
+      this.scrollhaciaabajo();
+      this.debeHacerScroll = false
+    }
+  }
   trackByMensaje(index: number, mensaje: MensajeChat){}
 
   formatearMensajeAsistente(mensaje:string){}
@@ -76,6 +96,50 @@ export class Chat {
       usuarioId: 'u1'
     },{
       id:'id6',
+      contenido:'1, 2, 3, 4, 5, 6, 7, 8, 9, 10',
+      tipo: 'Asistente',
+      fechaEnvio: new Date(ahora.getTime()),
+      estado: 'Enviado',
+      usuarioId: 'a1'
+    },
+    {
+      id:'id7',
+      contenido:'Hola eres el asistente?',
+      tipo: 'Usuario',
+      fechaEnvio: new Date(ahora.getTime()),
+      estado: 'Enviado',
+      usuarioId: 'u1'
+    },{
+      id:'id8',
+      contenido:'Hola soy tu asistente',
+      tipo: 'Asistente',
+      fechaEnvio: new Date(ahora.getTime()),
+      estado: 'Enviado',
+      usuarioId: 'a1'
+    },
+    {
+      id:'id9',
+      contenido:'Dime una palabra aleatoria',
+      tipo: 'Usuario',
+      fechaEnvio: new Date(ahora.getTime()),
+      estado: 'Enviado',
+      usuarioId: 'u1'
+    },{
+      id:'id10',
+      contenido:'español',
+      tipo: 'Asistente',
+      fechaEnvio: new Date(ahora.getTime()),
+      estado: 'Enviado',
+      usuarioId: 'a1'
+    },{
+      id:'id11',
+      contenido:'Dime los numeros del 1 al 10',
+      tipo: 'Usuario',
+      fechaEnvio: new Date(ahora.getTime()),
+      estado: 'Enviado',
+      usuarioId: 'u1'
+    },{
+      id:'id12',
       contenido:'1, 2, 3, 4, 5, 6, 7, 8, 9, 10',
       tipo: 'Asistente',
       fechaEnvio: new Date(ahora.getTime()),
